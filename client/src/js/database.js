7e973ba45c3ff9b2021c7a300053e6250c1a8e99
KNC2007 <cnkalbach@gmail.com>
2024-02-19 17:19:34 -0500

    fixed errors in console and made so can save and update notes after refresh

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,13 +14,12 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
@@ -36,13 +35,12 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
@@ -53,7 +51,7 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,7 +1,6 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Listen for the beforeinstallprompt event
 window.addEventListener('beforeinstallprompt', (event) => {
     console.log(event);
     event.preventDefault();
@@ -12,7 +11,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Logic for installing the PWA
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     console.log(promptEvent);
